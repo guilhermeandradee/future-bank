21b796d044b081fe725ec671732c281f66d7d734
Guilherme Andrade <96486731+guilhermeandradee@users.noreply.github.com>
2024-12-26 09:56:29 -0300

    logo, header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,13 @@ import "./Header.css"
 
 const Header = () => {
     return(
-        <div className="d-flex justify-content-between p-4 px-5">
-            <div>
-                <a className=" a-header" href="">Logo</a>
+        <div className="d-flex p-4 p-md-4 px-md-5 px-3 justify-content-between">
+            <div className="col-6 justitfy-content-center align-items-center">
+                <a className="w-100 h-100 justitfy-content-center align-items-center d-flex a-header" href=""><img className="col-3 col-sm-1 " src="/public/world-bank-logo-50FE63E4E3-seeklogo.com.png" alt="" /></a>
             </div>  
-            <div>
-                <a  className="me-5 a-header" href="/home">Home</a>
-                <a className=" a-header"  href="/">Create account</a>
+            <div className="col-6 row justify-content-end">
+                <a  className="col-12 col-sm-4 a-header text-md-end text-center" href="/home">Home</a>
+                <a className="col-12 col-sm-4 a-header text-md-end text-center"  href="/">Create account</a>
             </div> 
         </div>
     )
